ee61a2e50226dfa77b7a7e38025ec17050eb5c8e
unknown <tanjib.cse@gmail.com>
2022-05-07 03:28:01 +0600

    brand slider added

---
+++ src/Components/Pages/Home/BrandSlider/BrandSlider.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick/lib/slider';
+
+const BrandSlider = () => {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        cssEase: "linear"
+      };
+
+    return (
+        <div>
+            <h2>Brands</h2>
+            <div>
+        <Slider {...settings}>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
+        </div>
+    );
+};
+
+export default BrandSlider;
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Inventory from '../Inventory/Inventory';
+import BrandSlider from './BrandSlider/BrandSlider';
 import Slider from './Slider/Slider';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Slider/>
             <Inventory/>
+            <BrandSlider/>
         </div>
     );
 };
